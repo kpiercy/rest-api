02954da2292b88f470f3a3968a0e4a246a525d5a
Kraig Piercy <kapiercy11@gmail.com>
2023-04-07 02:57:06 -0400

    begin auth0 config

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const env = process.env.NODE_ENV || 'development'
+const config = require(__dirname + '/../config/config.js')[env]
+
+const jwt = require('express-jwt')
+const jwks = require('jwks-rsa')
+
+const jwtCheck = jwt({
+        secret: jwks.expressJwtSecret({
+            cache: true,
+            rateLimit: true,
+            jwksRequestsPerMinute: 300,
+            jwksUri: config.authProvider.jwks
+        }),
+    audience: config.baseUrl.url,
+    issuer: config.authProvider.url,
+    algorithms: ['RS256'],
+})
+
+module.exports = jwtCheck
